5b26c1e06ae6360afd11ea427b94bd5c0d9a5e6b
Josep M <jmcarreras@minsait.com>
2019-05-29 13:05:39 +0200

    commit 3 branca master

--- doc_1.R
+++ doc_1.R
@@ -3,3 +3,5 @@ Generem el primer document
 Afegim una nova línia de codi que la volem considerar en el mateix commit fent ammend
 
 Línia de codi que correspondria al segon commit
+
+Línia de codi corresponent al tercer commit
